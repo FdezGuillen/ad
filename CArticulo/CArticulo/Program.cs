695263c815aee9e6163e22a322bdd14c2c03cdae
FdezGuillen <contactaguillen@gmail.com>
2019-10-07 11:36:14 +0200

    Añadidos Nuevo(), Modificar(), Eliminar(), Consultar() y readDecimal() en CArticulo

--- CArticulo/CArticulo/Program.cs
+++ CArticulo/CArticulo/Program.cs
@@ -29,13 +29,36 @@ namespace CArticulo
                 + "3.- Eliminar artículo\n"
                 + "4.- Consultar artículo\n" +
                     "5.- Listar artículos \n" +
+                    "6. - Listar categorías \n " +
                     "0.- Salir");
             int entero = readInteger("Seleccione una opción: ");
 
             switch (entero) {
-                case 5:
-                    listar();
+                case 1:
+                    Nuevo();
                     break;
+
+                case 2:
+                    Modificar();
+                    break;
+
+                case 3:
+                    Eliminar();
+                    break;
+
+                case 4:
+                    Consultar();
+                    break;
+
+                case 5:
+                    Listar("articulo");
+                    break;
+
+                case 6:
+                    Listar("categoria");
+                    break;
+
+                    //todo: listarCategoria
             }
         }
 
@@ -52,31 +75,182 @@ namespace CArticulo
                     Console.WriteLine("Entrada no válida. Por favor, vuelve a intentarlo.\n");
                 }
             }
-
             return entero;
         }
 
-        public static void listar() {
+        public static Decimal readDecimal(String label) {
+            Decimal dec = -1;
+
+            while (dec == -1) {
+                try {
+                    Console.WriteLine(label);
+                    String linea = Console.ReadLine();
+                    dec = Decimal.Parse(linea);
+                }
+                catch {
+                    Console.WriteLine("Entrada no válida. Por favor, vuelve a intentarlo.\n");
+                }
+            }
+            return dec;
+        }
+
+
+        public static void Listar(string tabla) {
             IDbCommand dbCommand = dbConnection.CreateCommand();
-            dbCommand.CommandText = "select * from articulo";
+            dbCommand.CommandText = "select * from " + tabla;
             IDataReader dataReader = dbCommand.ExecuteReader();
 
             while (dataReader.Read()) {
-                Console.WriteLine("id={0} nombre={1} precio={2} id categoria={3}", dataReader["id"], dataReader["nombre"], 
-                    dataReader["precio"], dataReader["idCategoria"]);
+                if (tabla.Equals("articulo"))
+                    Console.WriteLine("id={0} nombre={1} precio={2} id categoria={3}", dataReader["id"], dataReader["nombre"], 
+                        dataReader["precio"], dataReader["idCategoria"]);
+                else if (tabla.Equals("categoria"))
+                    Console.WriteLine("id={0} nombre={1} ", dataReader["id"], dataReader["nombre"]);
             }
 
             dataReader.Close();
         }
 
-        public static void InsertValue() {
+        public static void Nuevo() {
+            string nombre;
+            Decimal precio;
+            int idCategoria;
+
             IDbCommand dbCommand = dbConnection.CreateCommand();
-            string nombre = "nuevo " + DateTime.Now;
-            string commandText = String.Format("insert into categoria(nombre) values ('{0}')", nombre);
-            dbCommand.CommandText = commandText;
-            dbCommand.ExecuteNonQuery();
+            dbCommand.CommandText = "insert into articulo(nombre, precio, idCategoria) values(@nombre, @precio, @idCategoria)";
+
+            try {
+                Console.WriteLine("Nombre del artículo: ");
+                nombre = Console.ReadLine();
+
+                precio = readDecimal("Precio del artículo: ");
+
+                idCategoria = readInteger("ID de la categoría: ");
+
+                DbCommandHelper.AddParameter(dbCommand, "nombre", nombre);
+                DbCommandHelper.AddParameter(dbCommand, "precio", precio);
+                DbCommandHelper.AddParameter(dbCommand, "idCategoria", idCategoria);
+
+                dbCommand.ExecuteNonQuery();
+
+            }
+            catch (Exception e){
+                Console.WriteLine("ERROR. {0}", e);
+            }
 
         }
+
+        public static void Modificar() {
+
+            int id;
+            string nombre;
+            Decimal precio;
+            int idCategoria;
+
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            dbCommand.CommandText = "update articulo " +
+            	"set nombre=@nombre, precio=@precio, idCategoria=@idCategoria " +
+            	"where id = @id";
+
+            try {
+                id = readInteger("ID del artículo a modificar: ");
+
+                Console.WriteLine("Datos actuales del artículo: ");
+                bool articuloExists = Consultar(id);
+
+                if (!articuloExists)
+                    throw new Exception();
+
+                Console.WriteLine("Nombre del artículo: ");
+                nombre = Console.ReadLine();
+
+                precio = readDecimal("Precio del artículo: ");
+
+                idCategoria = readInteger("ID de la categoría: ");
+
+                DbCommandHelper.AddParameter(dbCommand, "nombre", nombre);
+                DbCommandHelper.AddParameter(dbCommand, "precio", precio);
+                DbCommandHelper.AddParameter(dbCommand, "idCategoria", idCategoria);
+                DbCommandHelper.AddParameter(dbCommand, "id", id);
+
+                dbCommand.ExecuteNonQuery();
+
+            }
+            catch (Exception e) {
+                Console.WriteLine("ERROR. {0}", e);
+            }
+
+        }
+
+        public static void Eliminar() {
+
+            int id;
+
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            dbCommand.CommandText = "delete from articulo where id = @id";
+
+            try {
+                id = readInteger("ID del artículo a eliminar: ");
+
+                Console.WriteLine("Datos del artículo: ");
+                bool articuloExists = Consultar(id);
+
+                if (!articuloExists)
+                    throw new Exception();
+                    
+                DbCommandHelper.AddParameter(dbCommand, "id", id);
+
+                Console.WriteLine("¿Seguro que quieres eliminar este artículo? (Sí/No)");
+                string opcion = Console.ReadLine().ToLower();
+
+                if (opcion[0] == 's') {
+                    dbCommand.ExecuteNonQuery();
+                    Console.WriteLine("Artículo eliminado.");
+                }
+                else {
+                    Console.WriteLine("Operación cancelada.");
+                }
+
+            }
+            catch (FormatException) {
+                Console.WriteLine("Entrada incorrecta. Por favor, vuelve a intentarlo.");
+            }
+            catch (Exception e) {
+                Console.WriteLine("ERROR. {0}", e);
+            }
+
+        }
+
+        public static bool Consultar(int? id = null) {
+
+            bool recordExists = false;
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            dbCommand.CommandText = "select * from articulo where id=@id";
+
+            try {
+                if (id is null)
+                    id = readInteger("ID del artículo a modificar: ");
+
+                DbCommandHelper.AddParameter(dbCommand, "id", id);
+
+                IDataReader dataReader = dbCommand.ExecuteReader();
+                if (dataReader.Read()) {
+                    Console.WriteLine("id={0} nombre={1} precio={2} id categoria={3}", dataReader["id"], dataReader["nombre"],
+                        dataReader["precio"], dataReader["idCategoria"]);
+                    recordExists = true;
+                }
+                else {
+                    throw new Exception("No existe ningún artículo con este ID.");
+                }
+                dataReader.Close();
+            }
+            catch (Exception e) {
+                Console.WriteLine("ERROR. {0}", e);
+            }
+            return recordExists;
+        }
+
+       
 
         public static void ShowMetaInfo() {
             IDbCommand dbCommand = dbConnection.CreateCommand();
